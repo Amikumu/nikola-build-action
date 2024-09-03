4014343697ed960d131f16efa4476b78ca2b174f
Amikumu <105642861+Amikumu@users.noreply.github.com>
2024-09-03 19:07:34 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,8 +11,6 @@ echo "==> Preparing..."
 if ! $INPUT_DRY_RUN; then
     src_branch="$(python -c 'import conf; print(conf.GITHUB_SOURCE_BRANCH)')"
     dest_branch="$(python -c 'import conf; print(conf.GITHUB_DEPLOY_BRANCH)')"
-    git config --global user.email help@amikumu.com
-    git config --global user.name Amikumu
     git config --global --add safe.directory /github/workspace
     git remote add ghpages "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
     git fetch ghpages $dest_branch
